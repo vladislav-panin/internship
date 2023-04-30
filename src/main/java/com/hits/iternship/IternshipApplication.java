0016d12bb1a0b82da90dad9cb152d0712dd583ac
Anastasia Mandrakova <bucklia20@mail.ru>
2023-04-30 23:14:36 +0700

    proj

---
+++ src/main/java/com/hits/iternship/IternshipApplication.java
@@ -0,0 +1,13 @@
+package com.hits.iternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IternshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hits/iternship/IternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hits.iternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
