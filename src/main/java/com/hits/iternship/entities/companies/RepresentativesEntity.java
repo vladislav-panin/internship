7c8bf0ff4cdc46c0a6f47d7c53c215874e418453
Aleksey Voistinov <waywwwwalex5@rambler.ru>
2023-05-06 17:34:57 +0700

    gg wp

--- src/main/java/com/hits/iternship/entities/companies/RepresentativesEntity.java
+++ src/main/java/com/hits/iternship/entities/companies/RepresentativesEntity.java
@@ -1,6 +1,7 @@
 package com.hits.iternship.entities.companies;
 
 
+import com.hits.iternship.entities.contacts.ContactsEntity;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import jakarta.persistence.*;
@@ -26,11 +27,8 @@ public class RepresentativesEntity {
     @JoinColumn(name = "companies_id", referencedColumnName = "company_id") // Первое имя - хз что такое, второе - имя айди в классе репрезентетивитес
     CompanyEntity companyEntity;
 */
-    /*
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id", referencedColumnName = "id")
-    @Column(insertable=false, updatable=false)
-    ContactsEntity contacts;
-    */
 
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "representatives_id", referencedColumnName = "representative_id")
+    List<ContactsEntity> contacts;
 }
