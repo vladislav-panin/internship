b24bd9dc2965823377069480d4a976ba6270ee07
Anastasia Mandrakova <bucklia20@mail.ru>
2023-05-07 17:57:55 +0700

    добавил entity студент

--- src/main/java/com/hits/iternship/entities/students/StudentEntity.java
+++ src/main/java/com/hits/iternship/entities/students/StudentEntity.java
@@ -1,12 +1,15 @@
 package com.hits.iternship.entities.students;
 
 import com.hits.iternship.dto.StatusDto;
+import com.hits.iternship.entities.companies.CompanyEntity;
+import com.hits.iternship.entities.position.PositionEntity;
 import jakarta.persistence.Entity;
 import lombok.Data;
 import jakarta.persistence.*;
 
 import java.util.Date;
-/*
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "students")
@@ -14,26 +17,29 @@ public class StudentEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(insertable=false, updatable=false)
-    String id;
+    Integer studentId;
 
     String name;
 
     String image;
 
 
+    String status; // там статус какой-то странный но пока так
 
-    int interviewsCount;
+    Integer InterviewsCount;
 
-   // StatusDto status;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studentId", referencedColumnName = "studentId") //Один студент может пробовать себя во многих компаниях, но вот по итогу работать будет только в одной. Да и вообще в компаниях мы не отображаем студентов, так что тут оне ту мени
+    List<CompanyEntity> companies;
 
-   // List<CompanyEntity> companies;
 
-    //List<PositionEntity> positions;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studentId", referencedColumnName = "studentId") //Один студент может пробовать себя во многих компаниях, но вот по итогу работать будет только в одной. Да и вообще в компаниях мы не отображаем студентов, так что тут оне ту мени
+    List<PositionEntity> positions;
 
     Date lastActivity;
 
-    String position;
+
 
     // List<ContactsEntity> contacts;
 
@@ -41,4 +47,4 @@ public class StudentEntity {
 
 
 }
-*/
+
